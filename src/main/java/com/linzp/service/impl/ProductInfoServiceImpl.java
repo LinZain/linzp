a1d96d5f177c8b3de4de18e52ec1306c998797ea
Administrator <Administrator@WIN-01410121400>
2018-04-29 22:22:02 +0800

    linzp update sql

--- src/main/java/com/linzp/service/impl/ProductInfoServiceImpl.java
+++ src/main/java/com/linzp/service/impl/ProductInfoServiceImpl.java
@@ -103,7 +103,7 @@ public class ProductInfoServiceImpl implements ProductInfoService {
 	@Override
 	public List<Product> getListByActId(String actId) {
 		Map<String, Object> params = new HashMap<String, Object>();
-		String sql = "select promotion_price,pdt_pic,pdt_title,pdt_price,pdt_id from tb_product where pdt_id in(select pdt_id from tb_promoteProduct where act_id= :actId and pmtpdt_status='0')";
+		String sql = "select * where pdt_id in(select pdt_id from tb_promoteProduct where act_id= :actId and pmtpdt_status='0')";
 		params.put("actId", actId);
 
 		List<Product> list = (List<Product>) execOrderDAO.findEntityBySql(sql, params, Product.class);
