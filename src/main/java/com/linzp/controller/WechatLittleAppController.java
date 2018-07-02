c4996018c931e6cadffabb3fd8ab20fd25496c6f
LinZain <linzp_01@outlook.com>
2018-07-02 11:13:24 +0800

    linzp

--- src/main/java/com/linzp/controller/WechatLittleAppController.java
+++ src/main/java/com/linzp/controller/WechatLittleAppController.java
@@ -1,5 +1,7 @@
 package com.linzp.controller;
 
+import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
@@ -15,35 +17,42 @@ import net.sf.json.JSONObject;
 
 @Controller
 public class WechatLittleAppController {
-    private static Logger logger = Logger.getLogger(WechatLittleAppController.class);
+	private static Logger logger = Logger.getLogger(WechatLittleAppController.class);
 
-    @ResponseBody
-    @RequestMapping(value = "/onLogin", method = RequestMethod.GET)
-    public String getOpenId(HttpServletRequest request) {
-        String appid = request.getParameter("appid");
-        String secret = request.getParameter("secret");
-        String jsCode = request.getParameter("js_code");
-        String grantType = request.getParameter("grant_type");
-        if (appid == null || secret== null || jsCode == null || grantType == null) {
-            return "error";
-        }
-        String url = String.format(Constants.WECHAT_GET_OPENID_URL, appid, secret, jsCode, grantType);
-        String data = HttpUtil.get(url);
-        JSONObject jsonData = new JSONObject();
-        jsonData.put("data", data);
+	@ResponseBody
+	@RequestMapping(value = "/onLogin", method = RequestMethod.GET)
+	public String getOpenId(HttpServletRequest request) {
+		String appid = request.getParameter("appid");
+		String secret = request.getParameter("secret");
+		String jsCode = request.getParameter("js_code");
+		String grantType = request.getParameter("grant_type");
+		if (appid == null || secret == null || jsCode == null || grantType == null) {
+			return "error";
+		}
+		String url = String.format(Constants.WECHAT_GET_OPENID_URL, appid, secret, jsCode, grantType);
+		String data = HttpUtil.get(url);
+		JSONObject jsonData = new JSONObject();
+		jsonData.put("data", data);
 
-        return jsonData.toString();
-    }
-    
-    @ResponseBody
-    @RequestMapping(value = "/getNumber", method = RequestMethod.GET)
-    public String getNumber(HttpServletRequest request) {
-    	String stNum = request.getParameter("stnum");
-    	String url = String.format("http://119.23.221.161:8090/getNumber?stnum=%s", stNum);
-    	String data = HttpUtil.get(url);
-    	JSONObject jsonData = new JSONObject();
-    	jsonData.put("data", data);
-    	
-    	return jsonData.toString();
-    }
+		return jsonData.toString();
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/getNumber", method = RequestMethod.GET)
+	public String getNumber(HttpServletRequest request) {
+		String stNum = null;
+		try {
+			stNum = new String(request.getParameter("stnum").getBytes("utf-8"), "utf-8");
+
+			String url = String.format("http://119.23.221.161:8090/getNumber?stnum=%s", stNum);
+			String data = HttpUtil.get(url);
+			JSONObject jsonData = new JSONObject();
+			jsonData.put("data", data);
+
+			return jsonData.toString();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
