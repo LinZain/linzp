71fc4a56405e4279c5bc670aebaa1433a6b35898
LinZain <linzp_01@outlook.com>
2018-09-27 14:43:18 +0800

    linzp

--- src/main/java/com/linzp/util/FilterUtil.java
+++ src/main/java/com/linzp/util/FilterUtil.java
@@ -43,7 +43,7 @@ public class FilterUtil implements Filter {
         //System.out.println("============================SQL"+sql);
         //有sql关键字，跳转到error.html
         if (sqlValidate(sql)) {
-            throw new IOException("您发送请求中的参数中含有非法字符");
+            throw new IOException("Invaild symbol");
             //String ip = req.getRemoteAddr();
         } else {
             chain.doFilter(args0,args1);
@@ -54,10 +54,10 @@ public class FilterUtil implements Filter {
     protected static boolean sqlValidate(String str) {
         str = str.toLowerCase();//统一转为小写
         String badStr = "'|and|exec|execute|insert|select|delete|update|count|drop|*|%|chr|mid|master|truncate|" +
-                "char|declare|sitename|net user|xp_cmdshell|;|or|-|+|like'|and|exec|execute|insert|create|drop|" +
+                "char|declare|sitename|net user|xp_cmdshell|;|-|+|like'|and|exec|execute|insert|create|drop|" +
                 "table|from|grant|use|group_concat|column_name|" +
                 "information_schema.columns|table_schema|union|where|select|delete|update|order|by|count|*|" +
-                "chr|mid|master|truncate|char|declare|or|;|-|--|+|like|//|/|%|#";//过滤掉的sql关键字，可以手动添加
+                "chr|mid|master|truncate|char|declare|;|-|--|+|like|//|/|%|#";//过滤掉的sql关键字，可以手动添加
         String[] badStrs = badStr.split("\\|");
         for (int i = 0; i < badStrs.length; i++) {
             if (str.indexOf(badStrs[i]) >= 0) {
